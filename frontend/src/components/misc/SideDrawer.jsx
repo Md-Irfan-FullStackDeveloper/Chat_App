5e82d96c7da56b06db4571c070366fc641cb6af0
Md Irfan <irfanjunaid78081@gmail.com>
2023-03-11 21:52:58 +0530

    searc users sideDrawer is completed

--- frontend/src/components/misc/SideDrawer.jsx
+++ frontend/src/components/misc/SideDrawer.jsx
@@ -2,14 +2,23 @@ import {
   Avatar,
   Box,
   Button,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
   Flex,
+  Input,
   Menu,
   MenuButton,
   MenuDivider,
   MenuItem,
   MenuList,
+  Spinner,
   Text,
   Tooltip,
+  useDisclosure,
+  useToast,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { FiSearch } from "react-icons/fi";
@@ -17,25 +26,99 @@ import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
 import { ChatState } from "../../context/ChatProvider";
 import ProfileModal from "./ProfileModal";
 import { useNavigate } from "react-router";
+import ChatLoading from "../ChatLoading";
+import axios from "axios";
+import UserListItem from "../UserListItem";
 
 const SideDrawer = () => {
   const [search, setSearch] = useState();
-  const [searchResults, setSearchResults] = useState();
+  const [searchResult, setSearchResult] = useState();
   const [loading, setLoading] = useState(false);
   const [loadingChats, setLoadingChats] = useState(false);
-  const { user } = ChatState();
+  const { user, setSelectedChat, chats, setChats } = ChatState();
   const navigate = useNavigate();
+  const toast = useToast();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const handleLogout = () => {
     localStorage.removeItem("userInfo");
     navigate("/", { replace: true });
   };
 
+  // search functionality
+  const handleSearch = async () => {
+    if (!search) {
+      toast({
+        title: "Please Enter something in search",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "top-left",
+      });
+      return;
+    }
+
+    try {
+      setLoading(true);
+
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+
+      const { data } = await axios.get(
+        `http://localhost:8000/api/user?search=${search}`,
+        config
+      );
+
+      setLoading(false);
+      setSearchResult(data);
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: "Failed to Load the Search Results",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom-left",
+      });
+    }
+  };
+
+
+  const accessChat = async (userId) => {
+    try {
+      setLoadingChats(true);
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const { data } = await axios.post(`http://localhost:8000/api/chat`, { userId }, config);
+
+      if (!chats.find((c) => c._id === data._id)) setChats([data, ...chats]);
+      setSelectedChat(data);
+      setLoadingChats(false);
+      onClose();
+    } catch (error) {
+      toast({
+        title: "Error fetching the chat",
+        description: error.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom-left",
+      });
+    }
+  };
+  // console.log(searchResult);
   return (
     <>
       <Flex bg="white" w="100%" p="5px 10px" justify="space-between">
         <Tooltip label="Search users for chat" hasArrow placement="bottom-end">
-          <Button variant="ghost">
+          <Button variant="ghost" onClick={onOpen}>
             <FiSearch />
             <Text display={["none", "flex"]} px="4">
               Search users
@@ -43,10 +126,12 @@ const SideDrawer = () => {
           </Button>
         </Tooltip>
 
+        {/* logo */}
         <Text fontSize="2xl" color="goldenrod" fontWeight="600">
           SkyNet Chat App
         </Text>
 
+        {/* right side notification and profile*/}
         <Box>
           <Menu>
             <MenuButton p={1}>
@@ -73,6 +158,36 @@ const SideDrawer = () => {
           </Menu>
         </Box>
       </Flex>
+
+      <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth="1px">Search Users</DrawerHeader>
+          <DrawerBody>
+            <Flex pb={2}>
+              <Input
+                placeholder="Search by name or email"
+                mr={2}
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <Button onClick={handleSearch}>Go</Button>
+            </Flex>
+            {loading ? (
+              <ChatLoading />
+            ) : (
+              searchResult?.map((user) => (
+                <UserListItem
+                  key={user._id}
+                  user={user}
+                  handleFunction={() => accessChat(user._id)}
+                />
+              ))
+            )}
+            {loadingChats && <Spinner ml="auto" d="flex" />}
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
     </>
   );
 };
--- frontend/src/context/ChatProvider.js
+++ frontend/src/context/ChatProvider.js
@@ -1,23 +1,28 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const ChatContext = createContext();
 
 const ChatProvider = ({ children }) => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [user, setUser] = useState();
+  const [selectedChat, setSelectedChat] = useState();
+  const [chats, setChats] = useState([]);
 
   useEffect(() => {
     const userInfo = JSON.parse(localStorage.getItem("userInfo"));
     setUser(userInfo);
 
-    if(!userInfo) {
-      navigate('/')
+    if (!userInfo) {
+      navigate("/");
     }
   }, [navigate]);
 
   return (
-    <ChatContext.Provider value={{ user, setUser }}>
+    <ChatContext.Provider
+      value={{ user, setUser, selectedChat, setSelectedChat, chats, setChats }}
+    >
       {children}
     </ChatContext.Provider>
   );
