3d589c6d3ab575d731e4a64fb062274a7822dc04
Md Irfan <irfanjunaid78081@gmail.com>
2023-03-12 16:43:48 +0530

    Chatpage left side UI completed with create group functionality

--- frontend/src/components/misc/MyChats.jsx
+++ frontend/src/components/misc/MyChats.jsx
@@ -7,12 +7,12 @@ import ChatLoading from "../ChatLoading";
 import { AddIcon } from "@chakra-ui/icons";
 import { getSender } from "../../utils/chatLogics";
 
-const MyChats = () => {
+const MyChats = ({ fetchAgain }) => {
   const [loggedUser, setLoggedUser] = useState();
   const { user, selectedChat, setSelectedChat, chats, setChats } = ChatState();
   const toast = useToast();
 
-  const fetchChats = async ({fetchAgain}) => {
+  const fetchChats = async () => {
     // console.log(user._id);
     try {
       const config = {
@@ -21,7 +21,10 @@ const MyChats = () => {
         },
       };
 
-      const { data } = await axios.get("http://localhost:8000/api/chat", config);
+      const { data } = await axios.get(
+        "http://localhost:8000/api/chat",
+        config
+      );
       setChats(data);
     } catch (error) {
       toast({
@@ -42,28 +45,27 @@ const MyChats = () => {
 
   return (
     <Flex
-      d={{ base: selectedChat ? "none" : "flex", md: "flex" }}
+      display={{ base: selectedChat ? "none" : "flex", md: "flex" }}
       flexDir="column"
       alignItems="center"
       p={3}
       bg="white"
-      w={{ base: "100%", md: "31%" }}
+      w={{ base: "100%", md: "35%" }}
       borderRadius="lg"
       borderWidth="1px"
     >
       <Flex
         pb={3}
-        px={3}
         fontSize={{ base: "28px", md: "30px" }}
         fontFamily="Work sans"
         w="100%"
         justifyContent="space-between"
         alignItems="center"
       >
-        My Chats
+        <Text fontSize="1.5rem">My Chats</Text>
         <GroupChatModal>
           <Button
-            d="flex"
+            display="flex"
             fontSize={{ base: "17px", md: "10px", lg: "17px" }}
             rightIcon={<AddIcon />}
           >
@@ -73,7 +75,7 @@ const MyChats = () => {
       </Flex>
       <Flex
         flexDir="column"
-        p={3}
+        m="1rem 0"
         bg="#F8F8F8"
         w="100%"
         h="100%"
@@ -114,7 +116,7 @@ const MyChats = () => {
         )}
       </Flex>
     </Flex>
-  )
+  );
 };
 
 export default MyChats;
--- frontend/src/pages/Chatpage.jsx
+++ frontend/src/pages/Chatpage.jsx
@@ -1,5 +1,5 @@
 import { Box, Flex } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import ChatBox from "../components/misc/ChatBox";
 import MyChats from "../components/misc/MyChats";
 import SideDrawer from "../components/misc/SideDrawer";
@@ -7,14 +7,15 @@ import { ChatState } from "../context/ChatProvider";
 
 const Chatpage = () => {
   const { user } = ChatState();
+  const [fetchAgain, setFetchAgain] = useState(false)
 
   return (
     <Box w="100%">
       {user && <SideDrawer />}
 
-      <Flex border="1px solid red" justify="space-between">
-        {user && <MyChats />}
-        {user && <ChatBox />}
+      <Flex w='100%' m='25px 0' p='0 1.5rem' h='85vh' gap='1rem' justify="space-between">
+        {user && <MyChats fetchAgain={fetchAgain} />}
+        {user && <ChatBox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain}/>}
       </Flex>
     </Box>
   );
